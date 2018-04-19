373b67372b71a5f9f7cbc5faedd22f0741838dca
njays <njaysteele@gmail.com>
2018-04-19 16:55:09 +0100

    front end design complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navigation from './Components/Navigation/Navigation.js';
+import Logo from './Components/Logo/Logo.js';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './Components/Rank/Rank.js';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+  number: {
+    value: 100, 
+    density: { 
+      enable: true,
+      value_area: 800
+      }
+    }
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App"> 
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
-        {/*<Logo />
+        <Logo />
+        <Rank />
         <ImageLinkForm />
-        <FaceRecognition />*/}
+        {/*<FaceRecognition />*/}
       </div>
     );
   }
